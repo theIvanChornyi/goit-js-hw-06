c4cc4f3cf63d4190aefcfe9b7afc4ac179ac84bf
Ivan Chornyi <chorniyivan@icloud.com>
2022-08-21 22:20:31 +0300

    completed 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const modalForm = document.querySelector('.login-form');
+
+modalForm.addEventListener('submit', submit);
+
+
+function submit(event) {
+    event.preventDefault();
+
+    const modFormEl = event.currentTarget;
+
+    if (modFormEl.email.value === '' || modFormEl.password.value === '') {
+        return alert('Все поля должны быть заполнены!');
+    }
+
+    const userAnswear = {};
+    
+    userAnswear.email = modFormEl.email.value;
+    userAnswear.password = modFormEl.password.value;
+
+    console.log(userAnswear);
+
+    modFormEl.email.value = '';
+    modFormEl.password.value = '';
+}
+
 
