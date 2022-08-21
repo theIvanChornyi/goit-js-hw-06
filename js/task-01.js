2339d18e7b6f2de1a717a36da9496a45a73ff262
Ivan Chornyi <chorniyivan@icloud.com>
2022-08-21 14:33:31 +0300

    Complited task 2

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,15 @@ const categoriesList = document.querySelector('#categories');
 function firstScript(category) {
     const subCategories = category.children;
     console.log('Number of categories:', subCategories.length);
+    
+    // Пустой лог для более удобного визуального восприятия
+    console.log('');
 
     for (const subCategory of subCategories) {
         console.log('Category:', subCategory.firstElementChild.textContent);
         console.log('Elements:', subCategory.lastElementChild.children.length);
+
+        console.log(''); 
     }
     
 }
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+function addIngridientEl(ingredients) {
+  const newIngredientsEl = ingredients.map(ingredient => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.classList.add('item');
+    ingredientEl.textContent = ingredient;
+    return ingredientEl;
+  });
+
+  ingredientsListEl.append(...newIngredientsEl);
+  
+  // Просто коментарий в консоль
+  return `All done, check HTML`;
+}
+
+console.log(addIngridientEl(ingredients));
