670c5c1128ab75c3616d04596d4ebdbcaeef894f
Ivan Chornyi <chorniyivan@icloud.com>
2022-08-21 19:03:14 +0300

    complete 5 task

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+const welcomSign = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+};
+
+// welcomSign.input.addEventListener('input', (event) => {
+//     welcomSign.output.textContent = event.currentTarget.value;
+// });
+
+welcomSign.input.addEventListener('input', addText);
+
+function addText(event) {
+    welcomSign.output.textContent = event.currentTarget.value;
+    if (event.target.value === '') {
+        welcomSign.output.textContent = 'Anonymous';  
+    }
+}
