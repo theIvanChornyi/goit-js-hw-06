f995f30b517feababf58e3f782b1379bc8a7a7bb
Ivan Chornyi <chorniyivan@icloud.com>
2022-08-21 22:31:01 +0300

    Completed 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bkg: document.querySelector('body'),
+  changeBtn: document.querySelector('button'),
+  color: document.querySelector('.color'),
+};
+
+refs.changeBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  refs.bkg.style.backgroundColor = getRandomHexColor();
+  refs.color.textContent = getRandomHexColor();
+}
